6c296aa5d983ca9de9f4d389fb0f48bb4e6a34ab
WoodieMaster <holnid21@htl-kaindorf.at>
2023-04-23 19:39:16 +0200

    Initial commit

---
+++ src/main/java/net/wood_studios/wood_mod/WoodModClient.java
@@ -0,0 +1,11 @@
+package net.wood_studios.wood_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WoodModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/wood_studios/wood_mod/WoodModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.wood_studios.wood_mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WoodModDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
